46d2b1cb9535eb253b94d2ce323bdf69a9a41b36
Preetam Gopalasetty <preetamgopalasetty@gmail.com>
2024-04-02 16:08:43 +0530

    uploaded_project

---
+++ images.py
@@ -0,0 +1,20 @@
+techniche_prof = "yuvaan_prof.jpg"
+
+connect = "connect.jpg"
+
+connect2 = "connect2.jpg"
+
+more = "more.jpg"
+
+add_note = "add_note.jpg"
+
+more_connect = "more_connect.jpg"
+
+listing_link = "https://docs.google.com/spreadsheets/d/18FQaDi137zic0czS9rD_V1SdBiAiTRY4HPOAA1jNv8E/edit#gid=1787345625"
+
+marky_note = """Hi,
+I am Marketing lead of rover team of Robotics Club IITG.We are participating in ISRO Robotics Challenge'24 and looking for partnership opportunities.Could i get your contact number and email?"""
+
+send = "send.jpg"
+
+without_note = "no_note.jpg"
---
+++ main.py
@@ -0,0 +1,106 @@
+import pyautogui as pag
+import images
+
+pag.FAILSAFE = True
+
+
+cell_num = str(input("cell num: "))
+count = int(input("how many cells: "))
+
+# opening chrome profiles
+pag.hotkey("win", 'q',interval=0.25)
+pag.countdown(1)
+pag.hotkey("ctrl", 'a', "backspace")
+pag.countdown(1)
+pag.typewrite("google chrome",interval=0.02)
+pag.countdown(1)
+pag.click(x = (650,352), duration=1.13, tween = pag.easeInQuad)
+pag.countdown(3)
+
+# selecting techniche profile
+techniche_button = pag.locateCenterOnScreen(images.techniche_prof, confidence = 0.8) 
+pag.click(techniche_button, duration=1.28, tween = pag.easeOutQuad)
+pag.countdown(3)
+
+# opening listing excel sheet
+pag.hotkey("ctrl", 't')
+pag.countdown(3)
+pag.typewrite(images.listing_link, interval=0.08)
+pag.countdown(1)
+pag.press("enter")
+pag.countdown(4)
+
+# selecting cell num
+pag.moveTo(x = (930,160), duration = 0.85)
+pag.click()
+pag.keyDown("ctrl")
+pag.press('j')
+pag.keyUp("ctrl")
+pag.typewrite(cell_num, interval=0.1)
+pag.press("enter")
+
+while count>0:
+    
+    pag.countdown(3)
+    pag.hotkey("ctrl", 'c')
+    pag.countdown(2)
+    pag.hotkey("ctrl", 't')
+    pag.countdown(2)
+    pag.hotkey("ctrl", 'v')
+    pag.countdown(2)
+    pag.press("enter")
+    pag.countdown(10)
+
+    #clicking connect button
+    connect_button  = pag.locateCenterOnScreen(images.connect, confidence = 0.8,region = (50,50,1000,1000))
+    connect2_button = pag.locateCenterOnScreen(images.connect2, confidence = 0.8,region = (50,50,1000,1000))
+    more_button = pag.locateCenterOnScreen(images.more, confidence = 0.8,region = (50,50,1000,1000))
+    pag.countdown(1)
+
+    if connect_button:
+
+        pag.moveTo(connect_button, duration = 0.8)
+        pag.click()
+
+    elif connect2_button:
+
+        pag.moveTo(connect2_button, duration = 0.8)
+        pag.click()
+
+    else:
+
+        pag.leftClick(more_button, duration = 0.6)
+        more_connect_button = pag.locateCenterOnScreen(images.more_connect, confidence = 0.7)
+        pag.moveTo(more_connect_button, duration = 0.2)
+        pag.click()
+
+    #going to message box
+    pag.countdown(2)
+
+
+    # add_note_button = None
+    # while not add_note_button:
+    #     pag.countdown(1)
+    #     add_note_button = pag.locateCenterOnScreen(images.add_note, confidence = 0.8)
+    #     pag.moveTo(add_note_button, duration = 0.57)
+    #     pag.click()
+
+    # #typing message
+    # pag.countdown(1)
+    # pag.typewrite(images.marky_note, interval = 0.08)
+    
+    # #sending message
+    # send_button = pag.locateCenterOnScreen(images.send, confidence = 0.9)
+    send_button = pag.locateCenterOnScreen(images.without_note, confidence = 0.9)
+
+    pag.moveTo(send_button, duration = 0.5)
+    pag.click()
+
+    #closing tab and going for next cell
+    pag.countdown(3)
+    pag.hotkey("ctrl", 'w')
+    pag.countdown(3)
+    pag.press("DOWN")
+
+    count = count - 1
+    print(f"CELLS remaining {count}")
